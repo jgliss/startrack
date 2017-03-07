eb3b7767460a22f439f166f57396666f40859d91
adonath <axel.donath@mpi-hd.mpg.de>
2017-03-07 10:55:44 +0100

    Add __version__ attribute to startrack module

--- startrack/__init__.py
+++ startrack/__init__.py
@@ -1,1 +1,5 @@
+from pkg_resources import get_distribution
 from .code import *
+
+
+__version__ = get_distribution('startrack').version
---
+++ startrack/tests/test_version.py
@@ -0,0 +1,6 @@
+import startrack
+
+# TODO: add more tests
+def test_version():
+    assert startrack.__version__ == '0.1'
+    
